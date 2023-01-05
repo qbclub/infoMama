82ef3e008c642e6070088febb2a6cb49b9a3049d
regtyit <titov54275@gmail.com>
2023-01-05 13:51:46 +0400

    fast fix ForUsers

--- src/components/ForUsers.vue
+++ src/components/ForUsers.vue
@@ -3,17 +3,55 @@
 </script>
 
 <template>
-  <v-card class="mx-auto h-auto w-auto" variant="outlined">
-    <v-card-title class="text-h4 mb-1 font-weight-bold pa-4">
+  <v-card class="mx-auto h-auto w-75" variant="outlined">
+    <v-card-title class="text-h4 mb-1 font-weight-bold ma-4">
       Для пользователей
     </v-card-title>
 
-
-    <v-row justify="center" class="ma-4">
-      <v-col class="d-flex flex-column">
+    <v-row class="ma-4">
+      <v-col class="flex-column">
         <v-card
           width="auto"
           height="300"
+          class="mb-4"
+          title="This is a title"
+          subtitle="This is a subtitle"
+          text="This is content"
+          variant="outlined"
+        ></v-card>
+        <v-card
+          width="auto"
+          height="200"
+          class="mb-4"
+          title="This is a title"
+          subtitle="This is a subtitle"
+          text="This is content"
+          variant="outlined"
+        ></v-card>
+        <v-card
+          width="auto"
+          height="300"
+          title="This is a title"
+          subtitle="This is a subtitle"
+          text="This is content"
+          variant="outlined"
+        ></v-card>
+      </v-col>
+
+      <v-col class="flex-column">
+        <v-card
+          width="auto"
+          height="200"
+          class="mb-4"
+          title="This is a title"
+          subtitle="This is a subtitle"
+          text="This is content"
+          variant="outlined"
+        ></v-card>
+
+        <v-card
+          width="auto"
+          height="400"
           class="mb-4"
           title="This is a title"
           subtitle="This is a subtitle"
@@ -30,28 +68,7 @@
         ></v-card>
       </v-col>
 
-      <v-col class="d-flex flex-column" >
-        <v-card
-          width="auto"
-          height="200"
-          class="mb-4"
-          title="This is a title"
-          subtitle="This is a subtitle"
-          text="This is content"
-          variant="outlined"
-        ></v-card>
-
-        <v-card
-          width="auto"
-          height="300"
-          title="This is a title"
-          subtitle="This is a subtitle"
-          text="This is content"
-          variant="outlined"
-        ></v-card>
-      </v-col>
-
-      <v-col class="d-flex flex-column" >
+      <v-col class="flex-column">
         <v-card
           width="auto"
           height="300"
@@ -64,6 +81,15 @@
         <v-card
           width="auto"
           height="200"
+          class="mb-4"
+          title="This is a title"
+          subtitle="This is a subtitle"
+          text="This is content"
+          variant="outlined"
+        ></v-card>
+        <v-card
+          width="auto"
+          height="300"
           title="This is a title"
           subtitle="This is a subtitle"
           text="This is content"
@@ -71,7 +97,6 @@
         ></v-card>
       </v-col>
     </v-row>
-
   </v-card>
 </template>
 <style></style>
