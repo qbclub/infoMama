2ef379ac58cf49d65e33107a8bc6b766564b9ffb
JetGreen <grishadzyin@gmail.com>
2023-01-05 15:13:06 +0400

    add wordcloud

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,18 @@
 <script setup>
 import mainBanner from '../assets/images/main-banner.png'
+import wordcloud from '../assets/images/wordcloud-infomama.webp'
 </script>
 
 <template>
-  <v-parallax :src="mainBanner" height="60vh" class="d-flex text-center align-center">
+  <v-parallax :src="mainBanner" height="75vh">
     <v-row no-gutters>
+      <v-col cols="8">
+
+      </v-col>
       <v-col cols="4">
+        <img :src="wordcloud" style="object-fit: contain; max-height: 70vh" />
+      </v-col>
+      <!-- <v-col cols="4">
         <v-card class="mx-8 pa-3">
           <h3 class="mb-2">
             Роддом, больницы, узи, анализы, специалисты
@@ -43,7 +50,7 @@ import mainBanner from '../assets/images/main-banner.png'
             Барахолка и доставка продуктов
           </h3>
         </v-card>
-      </v-col>
+      </v-col> -->
     </v-row>
   </v-parallax>
 </template>
