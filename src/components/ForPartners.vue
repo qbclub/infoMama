7afef097707ccf5c16bf6d4ed7961a900b3be7f8
tooortttt <savelijsutov@gmail.com>
2023-01-05 14:47:21 +0400

    For business

--- src/components/ForPartners.vue
+++ src/components/ForPartners.vue
@@ -1,17 +1,89 @@
 <script setup>
+//
 </script>
 
 <template>
-  <v-card class="mx-auto w-75" variant="outlined">
-    <v-card-item>
-        <div class="text-h4 mb-1 font-weight-bold pa-3">
-          Для бизнеса
-        </div>
-    </v-card-item>
+  <v-card class="mx-auto h-auto w-75 rounded-xl big primary" variant="outlined">
     <v-container>
-    <v-row no-gutters>
-      
-    </v-row>
-  </v-container>
+      <v-card-title class="text-h4 font-weight-bold pl-0 mb-6" >
+        Для Бизнеса
+      </v-card-title>
+      <v-row dense>
+        <v-col class="flex-column">
+          <v-card width="auto" class="mb-4 rounded-xl" title="This is a title" variant="outlined">
+            <v-card-item>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaolas</h4>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaolas</h4>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaolas</h4>
+            </v-card-item>
+          </v-card>
+          <v-card width="auto" class="mb-4 rounded-xl" title="This is a title" variant="outlined">
+            <v-card-item>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaolas</h4>
+            </v-card-item></v-card
+          >
+          <v-card width="auto" class="rounded-xl" title="This is a title" variant="outlined">
+            <v-card-item>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaolas</h4>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaolas</h4>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaolas</h4>
+            </v-card-item></v-card
+          >
+        </v-col>
+
+        <v-col class="flex-column">
+          <v-card width="auto" class="mb-4 rounded-xl" title="This is a title" variant="outlined">
+            <v-card-item>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaolas</h4>
+            </v-card-item></v-card
+          >
+
+          <v-card width="auto" class="mb-4 rounded-xl" title="This is a title" variant="outlined">
+            <v-card-item>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaolas</h4>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaolas</h4>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaolas</h4>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaolas</h4>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaolas</h4>
+            </v-card-item></v-card
+          >
+          <v-card width="auto" class="rounded-xl" title="This is a title" variant="outlined">
+            <v-card-item>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaolas</h4>
+            </v-card-item></v-card
+          >
+        </v-col>
+
+        <v-col class="flex-column">
+          <v-card width="auto" class="mb-4 rounded-xl" title="This is a title" variant="outlined">
+            <v-card-item>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaolas</h4>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaolas</h4>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaolas</h4>
+            </v-card-item></v-card
+          >
+          <v-card width="auto" class="mb-4 rounded-xl" title="This is a title" variant="outlined">
+            <v-card-item>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaolas</h4>
+            </v-card-item></v-card
+          >
+          <v-card width="auto" class="rounded-xl" title="This is a title" variant="outlined">
+            <v-card-item>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaokas</h4>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaolas</h4>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaolas</h4>
+            </v-card-item></v-card
+          >
+        </v-col>
+      </v-row>
+    </v-container>
   </v-card>
 </template>
+<style>
+.v-card{
+  border: 2px solid black !important;
+}
+.big{
+  border: 2px solid !important;
+}
+</style>
