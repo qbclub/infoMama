e5c6347386763474f9f3e1b210b73b9e1dc17b6c
regtyit <titov54275@gmail.com>
2023-01-05 14:41:48 +0400

    another fix For Users

--- src/components/ForUsers.vue
+++ src/components/ForUsers.vue
@@ -3,100 +3,87 @@
 </script>
 
 <template>
-  <v-card class="mx-auto h-auto w-75" variant="outlined">
-    <v-card-title class="text-h4 mb-1 font-weight-bold ma-4">
-      Для пользователей
-    </v-card-title>
+  <v-card class="mx-auto h-auto w-75 rounded-xl big primary" variant="outlined">
+    <v-container>
+      <v-card-title class="text-h4 font-weight-bold pl-0 mb-6" >
+        Для пользователей
+      </v-card-title>
+      <v-row dense>
+        <v-col class="flex-column">
+          <v-card width="auto" class="mb-4 rounded-xl" title="This is a title" variant="outlined">
+            <v-card-item>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaolas</h4>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaolas</h4>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaolas</h4>
+            </v-card-item>
+          </v-card>
+          <v-card width="auto" class="mb-4 rounded-xl" title="This is a title" variant="outlined">
+            <v-card-item>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaolas</h4>
+            </v-card-item></v-card
+          >
+          <v-card width="auto" class="rounded-xl" title="This is a title" variant="outlined">
+            <v-card-item>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaolas</h4>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaolas</h4>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaolas</h4>
+            </v-card-item></v-card
+          >
+        </v-col>
 
-    <v-row class="ma-4">
-      <v-col class="flex-column">
-        <v-card
-          width="auto"
-          height="300"
-          class="mb-4"
-          title="This is a title"
-          subtitle="This is a subtitle"
-          text="This is content"
-          variant="outlined"
-        ></v-card>
-        <v-card
-          width="auto"
-          height="200"
-          class="mb-4"
-          title="This is a title"
-          subtitle="This is a subtitle"
-          text="This is content"
-          variant="outlined"
-        ></v-card>
-        <v-card
-          width="auto"
-          height="300"
-          title="This is a title"
-          subtitle="This is a subtitle"
-          text="This is content"
-          variant="outlined"
-        ></v-card>
-      </v-col>
+        <v-col class="flex-column">
+          <v-card width="auto" class="mb-4 rounded-xl" title="This is a title" variant="outlined">
+            <v-card-item>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaolas</h4>
+            </v-card-item></v-card
+          >
 
-      <v-col class="flex-column">
-        <v-card
-          width="auto"
-          height="200"
-          class="mb-4"
-          title="This is a title"
-          subtitle="This is a subtitle"
-          text="This is content"
-          variant="outlined"
-        ></v-card>
+          <v-card width="auto" class="mb-4 rounded-xl" title="This is a title" variant="outlined">
+            <v-card-item>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaolas</h4>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaolas</h4>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaolas</h4>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaolas</h4>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaolas</h4>
+            </v-card-item></v-card
+          >
+          <v-card width="auto" class="rounded-xl" title="This is a title" variant="outlined">
+            <v-card-item>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaolas</h4>
+            </v-card-item></v-card
+          >
+        </v-col>
 
-        <v-card
-          width="auto"
-          height="400"
-          class="mb-4"
-          title="This is a title"
-          subtitle="This is a subtitle"
-          text="This is content"
-          variant="outlined"
-        ></v-card>
-        <v-card
-          width="auto"
-          height="200"
-          title="This is a title"
-          subtitle="This is a subtitle"
-          text="This is content"
-          variant="outlined"
-        ></v-card>
-      </v-col>
-
-      <v-col class="flex-column">
-        <v-card
-          width="auto"
-          height="300"
-          class="mb-4"
-          title="This is a title"
-          subtitle="This is a subtitle"
-          text="This is content"
-          variant="outlined"
-        ></v-card>
-        <v-card
-          width="auto"
-          height="200"
-          class="mb-4"
-          title="This is a title"
-          subtitle="This is a subtitle"
-          text="This is content"
-          variant="outlined"
-        ></v-card>
-        <v-card
-          width="auto"
-          height="300"
-          title="This is a title"
-          subtitle="This is a subtitle"
-          text="This is content"
-          variant="outlined"
-        ></v-card>
-      </v-col>
-    </v-row>
+        <v-col class="flex-column">
+          <v-card width="auto" class="mb-4 rounded-xl" title="This is a title" variant="outlined">
+            <v-card-item>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaolas</h4>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaolas</h4>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaolas</h4>
+            </v-card-item></v-card
+          >
+          <v-card width="auto" class="mb-4 rounded-xl" title="This is a title" variant="outlined">
+            <v-card-item>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaolas</h4>
+            </v-card-item></v-card
+          >
+          <v-card width="auto" class="rounded-xl" title="This is a title" variant="outlined">
+            <v-card-item>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaokas</h4>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaolas</h4>
+              <h4>jdaijdaishdah3jawasjdhoashfqihfqjfjbcoashoasdfanlawdlaolas</h4>
+            </v-card-item></v-card
+          >
+        </v-col>
+      </v-row>
+    </v-container>
   </v-card>
 </template>
-<style></style>
+<style>
+.v-card{
+  border: 2px solid black !important;
+}
+.big{
+  border: 2px solid !important;
+}
+</style>
