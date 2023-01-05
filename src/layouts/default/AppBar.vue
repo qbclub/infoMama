7cbb98c4e6c2372f4962efab8ab1bf88c27e6180
Katya-com <katerina.yad.24@gmail.com>
2023-01-05 14:26:17 +0400

    road map+header

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,13 +1,31 @@
-<template>
-  <v-app-bar flat>
-    <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-6" />
-
-      Essentials Preset
-    </v-app-bar-title>
-  </v-app-bar>
-</template>
-
 <script setup>
-  //
+import { ref } from "vue";
+
+let drawer = ref(false);
+
 </script>
+
+<template>
+  <!-- <v-app-bar border class="d-flex">
+      <div>Пользователям</div>
+      <div>Партнёрам</div>
+      <div>Дорожная карта</div>
+  </v-app-bar> -->
+      <v-app-bar color="primary" prominent>
+        <v-app-bar-nav-icon
+          variant="text"
+          @click.stop="drawer = !drawer"
+        ></v-app-bar-nav-icon>
+
+        <v-toolbar-title> Основная информация </v-toolbar-title>
+
+        <v-spacer></v-spacer>
+      </v-app-bar>
+
+      <v-navigation-drawer v-model="drawer" temporary>
+        <v-list-item prepend-icon="mdi-view-dashboard" title="Пользователям" value="home"></v-list-item>
+        <v-list-item prepend-icon="mdi-forum" title="Партнёрам" value="home"></v-list-item>
+        <v-list-item prepend-icon="mdi-view-dashboard" title="Дорожная карта" value="home"></v-list-item>
+      </v-navigation-drawer>
+
+</template>
--- src/layouts/default/View.vue
+++ src/layouts/default/View.vue
@@ -1,9 +1,10 @@
+<script setup>
+import roadMap from "../../components/roadMap.vue";
+</script>
+
 <template>
   <v-main>
+    <roadMap />
     <router-view />
   </v-main>
 </template>
-
-<script setup>
-  //
-</script>
